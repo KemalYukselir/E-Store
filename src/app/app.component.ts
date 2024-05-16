23507e03571d9ade521bd95904d023616b8e775c
Kemal Yukselir <K.Yukselir123@gmail.com>
2024-05-16 23:24:28 +0100

    created test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule  } from '@angular/forms';
+import { TestComponent } from './test/test.component';  // Import the TestComponent
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
